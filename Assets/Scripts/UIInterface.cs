5aa7b332ce8696729ce5a5d98266bd4fc81d6324
kimyoungsu01 <kysmb200@gmail.com>
2025-07-31 14:17:45 +0900

    [UI&&GameManager.add] addScript

--- Assets/Scripts/UIInterface.cs
+++ Assets/Scripts/UIInterface.cs
@@ -5,27 +5,24 @@ using UnityEngine;
 
 public class UIInterface : MonoBehaviour
 {
-
     public int Score;
     public int hp;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void RefreshHpbar(/*펭귄*/)
     {
+        // GameManager에 있는 HP를 받아와서 HPbar에 표시해주는것
+        //if (collision.gameObject.CompareTag("장애물")) 
+        //{
+        //    Debug.Log("장애물");
+        //}
+    }
 
-        void hp(/*펭귄*/)
-        {
-            //if (collision.gameObject.CompareTag("장애물")) 
-            //{
-            //    Debug.Log("장애물");
-            //}
-        }
-
-        void Score(/*펭귄*/)
-        {
-            //if (collision.gameObject.CompareTag("펭귄"))
-            //{
-            //    Debug.Log("점수가 올라간다");
-            //}
-        }
-
+    public void RefreshScoreText(/*펭귄*/)
+    {
+        // GameManager에 있는 점수를 받아와서 점수를 표시해주는것
+        //if (collision.gameObject.CompareTag("펭귄"))
+        //{
+        //    Debug.Log("점수가 올라간다");
+        //}
+    }
 }
