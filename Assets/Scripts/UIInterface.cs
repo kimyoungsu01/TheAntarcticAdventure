81cb12a2a19c7c7c16325c2dba95090f8901ca4a
kimyoungsu01 <kysmb200@gmail.com>
2025-07-31 10:35:32 +0900

    [UI.add] addScene

--- Assets/Scripts/UIInterface.cs
+++ Assets/Scripts/UIInterface.cs
@@ -1,18 +1,29 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UIInterface : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public int Score;
+    public int hp;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        void hp(/*펭귄*/)
+        {
+            //if (collision.gameObject.CompareTag("장애물")) 
+            //{
+            //    Debug.Log("장애물");
+            //}
+        }
+
+        void Score(/*펭귄*/)
+        {
+            //if (collision.gameObject.CompareTag("펭귄"))
+            //{
+            //    Debug.Log("점수가 올라간다");
+            //}
+        }
     }
 }
