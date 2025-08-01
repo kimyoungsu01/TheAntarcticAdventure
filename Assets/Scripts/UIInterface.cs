046be72d045cea2afca6c32c187b404083c2fdd6
kimyoungsu01 <kysmb200@gmail.com>
2025-08-01 11:29:43 +0900

    [UI.add] 스크립트 내용 추가

--- Assets/Scripts/UIInterface.cs
+++ Assets/Scripts/UIInterface.cs
@@ -4,36 +4,27 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
+using TMPro;
 
-public class UIInterface : GameManager
+public class UIInterface : MonoBehaviour
 {
-    GameManager gameManager;
-    Player player;
-    // 
+    public TextMeshProUGUI scoreText; // UI 텍스트 연결
+    public Image hpBar; // 체력바 UI 연결 (선택)
 
-    public int Score;
-    public int hp;
+    private GameManager gameManager;
 
-    public void RefreshHpbar(/*펭귄*/)
+    private void Start()
     {
         gameManager = GameManager.Instance;
-        // GameManager에 있는 HP를 받아와서 HPbar에 표시해주는것
-        //if (collision.gameObject.CompareTag("장애물")) 
-        //{
-        //    Debug.Log("장애물");
-        //}
     }
 
-    public void RefreshScoreText(Collision2D collision)
+    public void RefreshHpbar()
+    {
+        // GameManager에 있는 HP를 받아와서 HPbar에 표시해주는것
+    }
+
+    public void RefreshScoreText()
     {
         // GameManager에 있는 점수를 받아와서 점수를 표시해주는것
-        
-        if (collision.gameObject.CompareTag("동전")) // 만약 동전 colision에 충돌한다면
-        {
-            gameManager.addscore(Score); // 점수가 올라간다
-            Text.print(Score);// 텍스트가 값을 반영한다
-            Debug.Log("점수가 올라간다");
-        }
-
     }
 }
